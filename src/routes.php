0f44820339f8becb8331617a7d092eb0d5317a53
Anderson Ramos <andsu.marks.r@gmail.com>
2025-11-17 01:37:05 -0300

    feat: add middleware and login route

--- src/routes.php
+++ src/routes.php
@@ -3,18 +3,24 @@ use Slim\App;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Src\Controllers\UsersController;
+use Src\Controllers\AuthController;
+use Src\Middlewares\AuthMiddleware;
 
 return function (App $app) {
     $app->get('/', function (Request $request, Response $response) {
         $response->getBody()->write('The App is Running on Slim! 🚀');
         return $response;
     });
+    
+    $app->group('/users', function ($group) {
+        $group->get('', [UsersController::class, 'getAll']);
+        $group->post('', [UsersController::class, 'create']);
+        $group->get('/by-email', [UsersController::class, 'getByEmail']);
+        $group->get('/{id}', [UsersController::class, 'getById']);
+        $group->put('/{id}', [UsersController::class, 'update']);
+        $group->delete('/{id}', [UsersController::class, 'delete']);
+        $group->put('/{id}/password', [UsersController::class, 'updatePassword']);
+    })->add(new AuthMiddleware($app->getResponseFactory()));
 
-    $app->get('/users', [UsersController::class, 'getAll']);
-    $app->post('/users', [UsersController::class, 'create']);
-    $app->get('/users/by-email', [UsersController::class, 'getByEmail']);
-    $app->get('/users/{id}', [UsersController::class, 'getById']);
-    $app->put('/users/{id}', [UsersController::class, 'update']);
-    $app->delete('/users/{id}', [UsersController::class, 'delete']);
-    $app->put('/users/{id}/password', [UsersController::class, 'updatePassword']);
+    $app->post('/login', [AuthController::class, 'login']);
 };
