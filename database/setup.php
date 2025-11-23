7c6e16e22cc7a6d3557477c83a0cc5b5cea77747
anderson Ramos <anderson.ramos@feltracoepertusatti.com>
2025-11-23 15:06:24 -0300

    fix: use the Database connection method on setup.php

--- database/setup.php
+++ database/setup.php
@@ -2,17 +2,13 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
+use Src\Database;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
 try {
-    $pdo = new PDO(
-        'pgsql:host=' . $_ENV['DB_HOST'] . ';port=' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_NAME'],
-        $_ENV['DB_USER'],
-        $_ENV['DB_PASS'],
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-    );
+    $pdo = Database::getConnection();
 
     $sqlFiles = glob(__DIR__ . '/*.sql');
 
